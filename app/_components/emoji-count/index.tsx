27802670f97ef9a8d317e04869d8968730fdfc46
caobainan <allencao@dofun.cn>
2023-12-06 21:12:31 +0800

    init ui

---
+++ app/_components/emoji-count/index.tsx
@@ -0,0 +1,27 @@
+import { Suspense } from "react"
+
+interface CountDisplayProps {
+    count?: number
+  }
+  
+  function CountDisplay({ count }: CountDisplayProps) {
+    return (
+      <p className="text-gray-500 mb-12 text-base animate-in fade-in slide-in-from-bottom-4 duration-1200 ease-in-out">
+        {count || "–––"} emojis generated and counting!
+      </p>
+    )
+  }
+  
+  async function AsyncEmojiCount() {
+    // const count = await getEmojisCount()
+  
+    return <CountDisplay count={100} />
+  }
+
+export function EmojiCount() {
+    return (
+        <Suspense fallback={<CountDisplay />}>
+            <AsyncEmojiCount />
+        </Suspense>
+    )
+}
